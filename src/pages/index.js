6f71854fd45dbbf9c5cd37aef7efa353df62db30
Robert Long <robert@robertlong.me>
2021-12-01 15:20:18 -0800

    OMI Website refactor part 1

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-// placeholder component that redirects to /home
-
-import React from 'react';
-import  { Redirect } from 'react-router-dom';
-export default function Home() {
-  return <Redirect to='/home' />;
-}
